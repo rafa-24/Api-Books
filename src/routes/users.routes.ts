eb4372f476e21af28449890b2bc1283fb895b18c
Rafa prieto <rafaprieto063@gmail.com>
2022-10-06 00:32:04 -0500

    api book terminada y funcional

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -5,6 +5,6 @@ import { create, login } from "../controllers/users.controllers";
 
 export const router = Router();
 
-router.post('/api/v1/register', create)
+router.post('/api/v1/auth/register', create)
 
-router.post('/api/v1/login', login)
+router.post('/api/v1/auth/login', login)
